dd496f0ba76e2f44de98598fb9b9db028d246ef6
hien nguyen <thuhiennguyen198@gmail.com>
2022-08-04 19:30:22 -0400

    add cards in battles

--- javascript.js
+++ javascript.js
@@ -61,7 +61,7 @@ const battle = () => {
        let  card1 = player1.shift();
         let card2 = player2.shift();
         battleCards = [card1, card2];
-        console.log({card1});
+        console.log({battleCards});
         //update html
         p1.innerHTML = showCard(card1, 0)
         p2.innerHTML = showCard(card2, 0)
@@ -77,37 +77,62 @@ const battle = () => {
 const showCard = (card, pos) => {
     const move = pos * 40;
     const cardColor = (card.suit == "diams" || card.suit == "hearts") ? "red" : "back"
-    const bCard = '<div style="color:'+cardColor+'">' + card.cardValue + ' &' + card.suit + '; </div>';
-    //  const bCard = `<div style="color:${cardColor}">' + ${card.cardValue} + ' &' + ${card.suit} + ';</div>`;
-    //  const testCard= '<div style="color:'+cardColor+'">' + card.cardValue + ' &' + card.suit + ';</div>';
-    // const coloredCard = `<div style=${cardColor}>${divarseInt(card.cardValue)} ${card.suit}</p>`
+    const bCard =
+      '<div class="hand" style="color:' +
+      cardColor +
+      "; margin-left:" +
+      move +
+      'px">' +
+      card.cardValue +
+      " &" +
+      card.suit +
+      "; </div>";
     return bCard
-    // return testCard
 }
 const checkWinner = (card1, card2, battleCards) => {
   if (card1.cardValue === card2.cardValue) {
     console.log("warBattle");
-    warBattle(player1, player2);
+    warBattle(battleCards);
   } else if (card1.cardValue > card2.cardValue) {
     player1 = player1.concat(battleCards);
-    score1.innerHTML = `SCORE: ${player1.length}`;
-    score2.innerHTML = `SCORE: ${player2.length}`;
-  } else {
+    console.log(player1.length);
+    console.log(player2.length);
+    score1.innerHTML = `Player1's score: ${player1.length}`;
+    score2.innerHTML = `Player2's score: ${player2.length}`;
+  } else if (card2.cardValue > card1.cardValue) {
     player2 = player2.concat(battleCards);
-    score1.innerHTML = `SCORE: ${player1.length}`;
-    score2.innerHTML = `SCORE: ${player2.length}`;
+    console.log(player1.length);
+    console.log(player2.length);
+    score1.innerHTML = `Player1's score: ${player1.length}`;
+    score2.innerHTML = `Player2's score: ${player2.length}`;
   }
 };
 
-const warBattle = (player1, player2) => {
-    battleCards.concat(player1.shift(), player2.shift());
-    newCard1 = player1.shift();
-    newCard2 = player2.shift();
-    checkWinner(newCard1, newCard2)
+const warBattle = (battleCards) => {
+   let card1;
+   let card2;
+   const pos = battleCards.length/2;
+    if(isGameOver){
+        console.log("Gameover");
+    } else {
+        for(let i = 0;i < 8;i++){
+          card1 = player1.shift();
+          battleCards = battleCards.concat(card1);
+          p1.innerHTML += showCard(card1,(pos+i));
+        }
+        for(let i = 0;i < 8;i++){
+          card2 = player2.shift();
+          battleCards = battleCards.concat(card2);
+          p2.innerHTML += showCard(card2,(pos+i));
+        }
+        console.log({battleCards});
+    }
+    checkWinner(card1, card2, battleCards)
 }
 const isGameOver = () => {
-    if(player1.length === 0 || player2.length === 0){
+    if(player1.length < 10 || player2.length < 10){
         gameOver = true;
+        console.log("Game over");
     }
 }
 
